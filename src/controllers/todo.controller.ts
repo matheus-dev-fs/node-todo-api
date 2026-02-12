0a5f9e9af027dd0b7426a94f55f7e896798f46f5
matheus-dev-fs <matheusmks10@gmail.com>
2026-02-12 17:02:35 -0300

    feat: implement logic to fetch all todos
    - Add getAll controller method to retrieve all Todo records from the database
    - Handle errors and return appropriate HTTP status codes and messages

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,10 +1,18 @@
 import { Request, RequestHandler, Response } from 'express';
-import { Todo } from '../models/todo.model';
+import { Todo, TodoInstance } from '../models/todo.model';
 
-export const getAll: RequestHandler = async (req: Request, res: Response) => {};
+export const getAll: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const list: TodoInstance[] = await Todo.findAll();
+        res.status(200).json({ list });
+    } catch (error: unknown) {
+        console.log('Sequelize Error', error);
+        res.status(500).json({ error: 'Ocorreu um erro interno.' });
+    }
+};
 
-export const create: RequestHandler = async (req: Request, res: Response) => {};
+export const create: RequestHandler = async (req: Request, res: Response): Promise<void> => { };
 
-export const update: RequestHandler = async (req: Request, res: Response) => {};
+export const update: RequestHandler = async (req: Request, res: Response): Promise<void> => { };
 
-export const remove: RequestHandler = async (req: Request, res: Response) => {};
+export const remove: RequestHandler = async (req: Request, res: Response): Promise<void> => { };
